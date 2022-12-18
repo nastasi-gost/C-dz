9a8b119ae56fcaa40906087180d2d70c7a4d3738
nastasi <nastasi.gost@mail.ru>
2022-12-18 10:01:17 +0300

    1

---
+++ Итоговое/Program.cs
@@ -0,0 +1,14 @@
+﻿string[] InputArray()
+{
+    Console.WriteLine("Введите размер массива");
+    int arraySize = Convert.ToInt32(Console.ReadLine());
+    string[] newArray = new string[arraySize];
+    for (int i = 0; i < arraySize; i++)
+    {
+        Console.Write($"Введите строку #{i + 1} -> ");
+        newArray[i] = Console.ReadLine();
+    }
+    return newArray;
+}
+
+string[] MyArray = InputArray ();
